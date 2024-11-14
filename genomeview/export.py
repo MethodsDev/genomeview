1d8d6acd9afe35dfcbb96b6aa0be47c4a27a23c1
GeorgescuC <GeorgescuC@users.noreply.github.com>
2024-11-14 17:26:26 -0500

    Fix naming of save() outputs

--- genomeview/export.py
+++ genomeview/export.py
@@ -34,16 +34,14 @@ def save(doc, output_path, output_format=None, requested_converter=None):
     if output_format is None:
         if output_path.lower().endswith(".pdf"):
             output_format = "pdf"
-            output_path += ".pdf"
         elif output_path.lower().endswith(".png"):
             output_format = "png"
-            output_path += ".png"
         elif output_path.lower().endswith(".svg"):
             output_format = "svg"
-            output_path += ".svg"
         else:
+            if not output_path.lower().endswith(".html"):
+                output_path += ".html"
             output_format = "html"
-            output_path += ".html"
 
     if output_format == "html":
         with open(output_path, "w") as f:
